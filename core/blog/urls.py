659d059fec7cd2ed7171cd5d4d219b12b18123b9
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-07-31 11:32:09 +0330

    update
    postlist redirect

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,9 +7,10 @@ app_name = "blog"
 
 urlpatterns = [
     # [...]
-    path('fb-index',views.indexview,name='fb-viewtest'),
+    #path('fb-index',views.indexview,name='fb-viewtest'),
     #path('cbv-index/', TemplateView.as_view(template_name='base.html',extra_context={"name":"ali"})),
     path('cbv-index',views.Indexview.as_view(),name='cbv-index'),
-    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='go-to-cv'),
+    path('post/',views.PostList.as_view(),name='post_view'),
+    path('go-to-index/<int:pk>', views.RedirectToAli.as_view(pattern_name='blog:cbv-index'), name='go-to-cv'),
 
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,12 +1,15 @@
-from typing import Any, Dict
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from typing import Any, Dict, Optional
+from django.db.models.query import QuerySet
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.generic.base import TemplateView,RedirectView
+from django.views.generic import ListView
 from .models import Post
+
 # Create your views here.
 
-def indexview(request):
-    name = 'ali'
-    return render(request,'base.html',{'name':name})
+# def indexview(request):
+#     name = 'ali'
+#     return render(request,'base.html',{'name':name})
 
 class Indexview(TemplateView):
     template_name = 'base.html'
@@ -16,3 +19,23 @@ class Indexview(TemplateView):
         context["name"] = "ali"
         context["post"] = Post.objects.all()
         return context
+# def redirectToAli(request):
+#     return redirect('https://alimahmoodi.net')
+
+class RedirectToAli(RedirectView):
+    url = 'https://alimahmoodi.net'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post,pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+    
+
+class PostList(ListView):
+    #model = Post
+    #queryset = Post.objects.all()
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.all()
+        return posts
