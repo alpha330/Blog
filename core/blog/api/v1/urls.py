b1ab9d7ff065ff59cf95a8c4f39cf62226d0b12e
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-14 22:01:17 +0330

    update SE6 ClassbaseView Framework
    7.move from generic vies to viesets

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -7,8 +7,8 @@ from . import views
 app_name = "api_v1"
 
 urlpatterns = [
-    # path('/post',views.postlist,name='api_post_view'),
-    path('/post',views.PostList.as_view(),name="post_list_api"),
-    # path('/post/<int:id>/',views.post_details,name='api_post_detail_view'),
-    path('/post/<int:pk>/',views.PostDetail.as_view(),name='api_post_detail_view'),
+    # path('/post',views.PostList.as_view(),name="post_list_api"),
+    # path('/post/<int:pk>/',views.PostDetail.as_view(),name='api_post_detail_view'),
+    path('/post',views.PostViewSet.as_view({'get':'list','post':'create'}),name="post_list_api"),
+    path('/post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update'}),name='api_post_detail_view'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
 from blog.models import Post
 from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
@@ -81,4 +82,29 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     
+class PostViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+    
+    def list(self,request):
+        queryset = self.queryset
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    def retrieve(self,request,pk=None):
+        post = get_object_or_404(self.queryset,pk=pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+    def update(self,request,pk=None):
+        post = get_object_or_404(self.queryset,pk=pk)
+        serializer=self.serializer_class(post)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
     
