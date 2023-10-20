42fb0db703b9d0d07e81ae17c8010a9c10966567
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-20 22:23:55 +0330

    update SE6 ClassbaseView Framework
    to representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    category = serializers.SlugRelatedField(many=False,slug_field="name",queryset=Category.objects.all())
     class Meta:
         model = Post
         fields = ["id","title","updated_date","snippet","published_date","status","relative_url","author","category","content","image","absolute_url"]
@@ -16,6 +17,8 @@ class PostSerializer(serializers.ModelSerializer):
     def get_absolute_url(self,obj):
         request= self.context.get('request')
         return request.build_absolute_uri(obj.pk)
+    def to_representation(self,instance):
+        return super().to_representation(instance)
 class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
