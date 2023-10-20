1eab370c55d899ad82fda307c99e36da1d8d654f
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-20 22:49:54 +0330

    update SE6 ClassbaseView Framework

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -4,11 +4,15 @@ from blog.models import Post,Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ["name","id"]
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
     absolute_url = serializers.SerializerMethodField()
-    category = serializers.SlugRelatedField(many=False,slug_field="name",queryset=Category.objects.all())
     class Meta:
         model = Post
         fields = ["id","title","updated_date","snippet","published_date","status","relative_url","author","category","content","image","absolute_url"]
@@ -18,9 +22,15 @@ class PostSerializer(serializers.ModelSerializer):
         request= self.context.get('request')
         return request.build_absolute_uri(obj.pk)
     def to_representation(self,instance):
-        return super().to_representation(instance)
-class CategorySerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Category
-        fields = ["name","id"]
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop("snippet",None)
+            rep.pop("absolute_url",None)
+            rep.pop("relative_url",None)
+        else :
+            rep.pop("content",None)
+            
+        rep["category"] = CategorySerializer(instance.category).data
+        
+        return rep
