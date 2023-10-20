fbc4bf7e32501b33a31bb8065a8a5d1b1eff619b
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-20 23:08:57 +0330

    update SE6 ClassbaseView Framework
    user get

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from blog.models import Post,Category
+from accounts.models import Profile
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
@@ -16,7 +17,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["id","title","updated_date","snippet","published_date","status","relative_url","author","category","content","image","absolute_url"]
-        read_only_fields = ["content"]
+        read_only_fields = ["content","author"]
         
     def get_absolute_url(self,obj):
         request= self.context.get('request')
@@ -29,8 +30,12 @@ class PostSerializer(serializers.ModelSerializer):
             rep.pop("absolute_url",None)
             rep.pop("relative_url",None)
         else :
-            rep.pop("content",None)
-            
-        rep["category"] = CategorySerializer(instance.category).data
-        
+            rep.pop("content",None)    
+        rep["category"] = CategorySerializer(instance.category,context={"request":request}).data
         return rep
+    def create(self, validate_date):
+        validate_date["author"]=Profile.objects.get(user__id = self.context.get("request").user.id)
+        return super().create(validate_date)
+    
+    def update(self, instance, validate_date):
+        return super().update(instance, validate_date)
