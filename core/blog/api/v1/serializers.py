74cdcf7b37fe2957c4de169556a1ccc557596df8
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-20 19:09:00 +0330

    update SE6 ClassbaseView Framework
    relative def in serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -7,10 +7,15 @@ from blog.models import Post,Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_url = serializers.URLField(source="get_absolute_api_url",read_only=True)
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ["id","title","updated_date","snippet","published_date","status","relative_url","author","category","content","image"]
+        fields = ["id","title","updated_date","snippet","published_date","status","relative_url","author","category","content","image","absolute_url"]
         read_only_fields = ["content"]
+        
+    def get_absolute_url(self,obj):
+        request= self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
