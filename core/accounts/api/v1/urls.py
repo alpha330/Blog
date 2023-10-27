90cc34e9aa1dc36f75b02e6b043bcd36652df190
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-27 17:40:47 +0330

    Authentication Api S:7
    7-Customize Token Authentication

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api-v1"
 
 urlpatterns = [
     # Registration
     path("registration/",views.RegistrationApiView.as_view(),name="registration"),
-    path("token/login",ObtainAuthToken.as_view(),name="token-login")
+    path("token/login/",views.CustomAuthToken.as_view(),name="token-login")
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics,status
 from rest_framework.response import Response
-from accounts.api.v1.serializer import RegistrationSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from accounts.api.v1.serializer import RegistrationSerializer,CustomAuthTokenSerializer
 
 
 class RegistrationApiView(generics.GenericAPIView):
@@ -16,3 +18,19 @@ class RegistrationApiView(generics.GenericAPIView):
             }
             return Response(data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomAuthToken(ObtainAuthToken):
+    
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
