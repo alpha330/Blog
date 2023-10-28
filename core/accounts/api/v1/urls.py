f29c8222ccac12f2bf3b3399a370b3f989e2845b
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-10-28 09:19:28 +0330

    Authentication Api S:7
    13.postman with JWT

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 # from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api-v1"
@@ -15,8 +20,10 @@ urlpatterns = [
     
     # Login Token
     path("token/login/",views.CustomAuthToken.as_view(),name="token-login"),
-    path("token/logout/",views.CustomDiscardAuthToken.as_view(),name="token-logout")
+    path("token/logout/",views.CustomDiscardAuthToken.as_view(),name="token-logout"),
     
     # Login JWT
-    
+    path("jwt/create/",TokenObtainPairView.as_view(),name="jwt-create"),
+    path("jwt/refresh/",TokenRefreshView.as_view(),name="jwt-refresh"),
+    path("jwt/verify/",TokenVerifyView.as_view(),name="jwt-verify"),
 ]
