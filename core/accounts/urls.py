8c3c1d7fee1667928142984707ddb07fb1d41c4f
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-12-10 22:53:53 +0330

    cachserver S:16

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-
+from accounts import views
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("test/",views.test,name="test"),
     path("api/v1/", include("accounts.api.v1.urls"), name="api_list_post"),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,8 @@
-# from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+import requests
 # Create your views here.
+
+def test(request):
+    Response = requests.get("https://a89d010f-1b29-4211-b34b-0ce6c63ed80e.mock.pstmn.io/test/delay/5")
+    return JsonResponse(Response.json())
