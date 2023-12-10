841db5de3b6e753508b14af5d2360e706d2b570d
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-12-10 23:22:21 +0330

    redis django cache  S:16

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from django.core.cache import cache
 import requests
-# Create your views here.
+from django.views.decorators.cache import cache_page
 
+# Create your views here.
+@cache_page(60 * 15)
 def test(request):
     Response = requests.get("https://a89d010f-1b29-4211-b34b-0ce6c63ed80e.mock.pstmn.io/test/delay/5")
     return JsonResponse(Response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -216,6 +216,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://redis:6379/2",
+        "TIMEOUT":"60",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
