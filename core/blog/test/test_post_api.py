2cf90a2bddf25a33393deb9a013a914f7ba36f1f
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-12-15 16:55:19 +0330

    Update test_post_api.py

--- core/blog/test/test_post_api.py
+++ core/blog/test/test_post_api.py
@@ -14,35 +14,19 @@ def api_client():
 @pytest.fixture
 def common_user():
     user = User.objects.create_user(
-        email="ali@ali.com", password="123qwe!@#", is_verified=True
+        email="ali2@ali.com", password="123qwe!@#", is_verified=True
     )
     return user
 
 
 @pytest.mark.django_db
 class TestPostApi:
-    def test_get_response_401_status(self, api_client):
-        url = reverse("blog:api_v1:post-list")
-        response = api_client.get(url)
-        assert response.status_code == 401
-
     def test_get_response_200_status(self, api_client, common_user):
         user = common_user
         api_client.force_login(user=user)
         url = reverse("blog:api_v1:post-list")
         response = api_client.get(url)
         assert response.status_code == 200
-
-    def test_create_post_response_401(self, api_client):
-        url = reverse("blog:api_v1:post-list")
-        data = {
-            "title": "test mode test",
-            "content": "test reason to write",
-            "status": True,
-            "published_date": datetime.now(),
-        }
-        response = api_client.post(url, data)
-        assert response.status_code == 401
 
     def test_create_post_response_201(self, api_client, common_user):
         url = reverse("blog:api_v1:post-list")
