de7ee195decbfb51c891c32b9ea4ec4da57cf9d6
alpha330 <43549306+alpha330@users.noreply.github.com>
2023-12-15 16:38:05 +0330

    update standardize with flake8

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,7 +4,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
-    path("test/",views.test,name="test"),
+    path("test/", views.test, name="test"),
     path("api/v1/", include("accounts.api.v1.urls"), name="api_list_post"),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from django.core.cache import cache
+from django.http import JsonResponse
 import requests
 from django.views.decorators.cache import cache_page
 
 # Create your views here.
+
+
 @cache_page(60 * 15)
 def test(request):
     Response = requests.get("https://a89d010f-1b29-4211-b34b-0ce6c63ed80e.mock.pstmn.io/test/delay/5")
